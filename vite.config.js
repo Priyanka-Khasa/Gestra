5314dfdf77b3fd3e9ae6841b29a798c791f3310b
Priyanka Khasa <priyankakhasa937@gmail.com>
2026-06-09 10:54:18 +0530

    removed electron and wanted to make it a real time project

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,8 @@
-import { defineConfig } from 'vite';
-import electron from 'vite-plugin-electron';
-import renderer from 'vite-plugin-electron-renderer';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    electron([
-      {
-        entry: 'electron/main.cjs',
-        vite: {
-          build: {
-            lib: {
-              entry: 'electron/main.cjs',
-              formats: ['cjs'],
-            },
-            rollupOptions: {
-              external: ['electron', '@nut-tree-fork/nut-js'],
-              output: {
-                entryFileNames: '[name].cjs',
-              },
-            },
-          },
-        },
-      },
-      {
-        entry: 'electron/preload.cjs',
-        vite: {
-          build: {
-            lib: {
-              entry: 'electron/preload.cjs',
-              formats: ['cjs'],
-            },
-            rollupOptions: {
-              external: ['electron'],
-              output: {
-                entryFileNames: '[name].cjs',
-              },
-            },
-          },
-        },
-      },
-    ]),
-    renderer(),
-  ],
+  server: {
+    port: 5173,
+    open: true
+  }
 });
